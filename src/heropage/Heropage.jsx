bb983e767e2ff73478d9dc10fa7e7ec6d4b89d71
harshalranjhani <ranjhaniharshal@gmail.com>
2023-06-07 20:19:33 +0530

    integrate mapbox

--- src/heropage/Heropage.jsx
+++ src/heropage/Heropage.jsx
@@ -11,24 +11,24 @@ w-1/2
 `;
 const Heropage = () => (
   <div className=" flex w-full flex-row font-serif">
-    <div className={`${divStyle} w-3/5`}>
-      <h1 className="pb-5 text-8xl font-bold">Finding the missing</h1>
-      <p className="px-5 font-semibold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nam quas,
-        distinctio tempora ratione dolore ab rerum fugit tenetur, itaque minima
-        similique obcaecati quis laborum, omnis molestias deleniti nisi autem!
-      </p>
-      <div className="mt-10 flex justify-center">
-        <Button
-          type="button"
-          variant="contained"
-          className={`${buttonStyle}`}
-          href="/"
-        >
-          Login
-        </Button>
+      <div className={`${divStyle} w-3/5`}>
+        <h1 className="pb-5 text-8xl font-bold">Finding the missing</h1>
+        <p className="px-5 font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure nam quas,
+          distinctio tempora ratione dolore ab rerum fugit tenetur, itaque minima
+          similique obcaecati quis laborum, omnis molestias deleniti nisi autem!
+        </p>
+        <div className="mt-10 flex justify-center">
+          <Button
+            type="button"
+            variant="contained"
+            className={`${buttonStyle}`}
+            href="/"
+          >
+            Login
+          </Button>
+        </div>
       </div>
-    </div>
 
     <div className={`${divStyle} h-96 w-2/5`}>
       <Sexy />
